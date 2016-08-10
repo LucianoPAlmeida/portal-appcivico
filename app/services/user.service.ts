e2b9d2ae32d4f4aacd5890ca3ed31a9b4c568074
LucianoAlmeida <passos.luciano@outlook.com>
2016-08-10 11:26:20 -0300

    login alterado

--- app/services/user.service.ts
+++ app/services/user.service.ts
@@ -12,7 +12,9 @@ export /**
 class UserService {
 
     registerDeveloperURL = "http://localhost:8888/appcivico-server/register.php";
-    loginUrl = "http://localhost:8888/appcivico-server/loginform.php";
+    //loginUrl = "http://localhost:8888/appcivico-server/loginform.php";
+    loginUrl = "http://mobile-aceite.tcu.gov.br/appCivicoRS/rest/pessoas/autenticar";
+    userProfileURL = "";
 
     constructor(private http: Http, private cookieService: CookieService){}
 
@@ -33,15 +35,24 @@ class UserService {
        
     }
 
-    authenticate(email: string, password: string): Observable<boolean> {
-        var headers = new Headers({'Content-Type' : 'application/x-www-form-urlencoded', 'accept' : 'application/json'});
-        var body = 'email='+ email + '&password=' + password;
-        return this.http.post(this.loginUrl,body, {headers: headers}).map(response => {
+    authenticate(email: string, password: string): Observable<void> {
+        // var headers = new Headers({'Content-Type' : 'application/x-www-form-urlencoded', 'accept' : 'application/json'});
+        // var body = 'email='+ email + '&password=' + password;
+        // return this.http.post(this.loginUrl,body, {headers: headers}).map(response => {
+            // var token : string = response.headers.get('appToken');
+        //     var developer : Developer = response.json();
+            // this.setLoggedUserSession(new UserSession(token,developer));
+            // return true;
+        // });
+
+        var headers = new Headers({'email' : email, 'senha' : password});
+        return this.http.get(this.loginUrl,{headers: headers}).map((response: Response)=>{
             var token : string = response.headers.get('appToken');
-            var developer : Developer = response.json();
+            var developer : Developer = this.jsonToDeveloper(response.json());
             this.setLoggedUserSession(new UserSession(token,developer));
-            return true;
+            return;
         });
+
     }
 
     setLoggedUserSession(currentSession: UserSession){
@@ -56,6 +67,12 @@ class UserService {
         return this.cookieService.getObject('user_session') as UserSession;
     }
 
+    updateUserOnSession(developer: Developer){
+        var session = this.currentSession();
+        session.currentDeveloper = developer;
+        this.setLoggedUserSession(session);
+    }
+
     hasAuthenticatedUser(): boolean {
         return this.currentSession() != null;
     }
@@ -63,7 +80,9 @@ class UserService {
 
     private jsonToDeveloper(json: any ): Developer{
       var developer : Developer = new Developer();
-      developer.name = json[''];
+      developer.name = json['nomeUsuario'];
+      developer.email = json['email'];
+      developer.cod = json['cod'];
       return developer;
     }
 }
