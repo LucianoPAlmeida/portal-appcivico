bb3d6645747e0501676cc5d77b86a5f0bae4b03c
LucianoAlmeida <passos.luciano@outlook.com>
2016-08-15 17:01:37 -0300

    router bug fix

--- app/main.ts
+++ app/main.ts
@@ -2,9 +2,10 @@ import {bootstrap}    from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {appRouterProviders} from './routes/app.routes';
 import {enableProdMode} from '@angular/core';
+import { Location, LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 
 
 enableProdMode();
 
-bootstrap(AppComponent,[appRouterProviders]);
+bootstrap(AppComponent,[appRouterProviders, {provide: LocationStrategy, useClass: HashLocationStrategy}]);
